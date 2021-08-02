0ac3c93873ccb8c6d96d707317cedb5be2b55525
pbrownsack <pqtbrown@gmail.com>
2021-08-02 17:00:53 -0500

    Add passport.js support

--- server/app.js
+++ server/app.js
@@ -2,16 +2,64 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const app = express();
+const sql = require("./mysql");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/User");
 
 require("dotenv").config();
+
+/* MIDDLEWARE CONFIGRATION */
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'scatdadobapbedoop' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const v1Route = require("./routes/v1/index");
 app.use("/v1", v1Route);
+
+/* PASSPORT CONFIGURATION */
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+})
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+    // TODO: Find user by username and verify password
+    try {
+        const user = await User.find(username);
+
+        if (!user) {
+            return done(null, false, { error: "User does not exist!" });
+        }
+
+        const verified = User.verifyPassword(user, password);
+
+        if (!verified) {
+            return done(null, false, { error: "Incorrect password!" });
+        }
+
+        return done(null, user);
+    } catch (err) {
+        return done(err);
+    }
+}))
+
+/* LISTEN SERVER INIT */
 
 app.listen(process.env.PORT_NO, () => {
     console.log("[CLEM] Process running on " + process.env.PORT_NO);
