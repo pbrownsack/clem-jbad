de243af47e99e359af77693aff25ad6672c630b9
pbrownsack <pqtbrown@gmail.com>
2021-08-04 00:15:17 -0500

    Project model advancements

--- server/models/Project.js
+++ server/models/Project.js
@@ -3,15 +3,31 @@ const sql = require("../mysql");
 const Project = {};
 
 Project.createNew = (fields) => new Promise((resolve, reject) => {
-
+    sql.query("INSERT INTO projects (name, customer_id, description, date_started, date_finished, price_per_ft, final_footage VALUES (?, ?, ?, CURRENT_DATE(), ?, ?, ?)", [
+        fields.name,
+        fields.customer_id,
+        fields.description,
+        null,
+        fields.price_per_ft,
+        null
+    ], (err, result) => {
+        if (err) return reject(err.code);
+        resolve(result);
+    })
 })
 
 Project.findById = (id) => new Promise((resolve, reject) => {
-
+    sql.query("SELECT * FROM projects WHERE id = ? LIMIT 1", [id], (err, results) => {
+        if (err) return reject(err.code);
+        resolve(results[0]);
+    })
 })
 
 Project.findByCustomerId = (customerId) => new Promise((resolve, reject) => {
-
+    sql.query("SELECT * FROM projects WHERE customer_id = ?", [customerId], (err, results) => {
+        if (err) return reject(err.code);
+        resolve(results);
+    })
 })
 
 Project.getTotalHours = (id) => new Promise((resolve, reject) => {
@@ -43,7 +59,10 @@ Project.getAllHours = (id) => new Promise((resolve, reject) => {
 })
 
 Project.remove = (id) => new Promise((resolve, reject) => {
-
+    sql.query("DELETE FROM projects WHERE id = ?", [id], (err, result) => {
+        if (err) return reject(err.code);
+        resolve(result);
+    })
 })
 
 module.exports = Project;
