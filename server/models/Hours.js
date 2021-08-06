289727e558c5fe630cc5b88b1972c662a382a8bb
pbrownsack <pqtbrown@gmail.com>
2021-08-06 00:36:01 -0500

    Add more functionality to Hours model

--- server/models/Hours.js
+++ server/models/Hours.js
@@ -15,7 +15,50 @@ Hours.createNew = (fields) => new Promise((resolve, reject) => {
     })
 })
 
-// TODO: Add more hourly time operations
+Hours.findById = (id) => new Promise((resolve, reject) => {
+    sql.query("SELECT * FROM hours WHERE id = ? LIMIT 1", [id], (err, results) => {
+        if (err) return reject(err.code);
+        resolve(results[0]);
+    })
+})
+
+Hours.getAll = (userId) => new Promise((resolve, reject) => {
+    if (userId === undefined) {
+        sql.query("SELECT * FROM hours", (err, results) => {
+            if (err) return reject(err.code);
+            resolve(results);
+        })
+    } else {
+        sql.query("SELECT * FROM hours WHERE user_id = ?", [userId], (err, results) => {
+            if (err) return reject(err.code);
+            resolve(results);
+        })
+    }
+})
+
+Hours.getByProject = (projectId) => new Promise((resolve, reject) => {
+    sql.query("SELECT * FROM hours WHERE project_id = ?", [projectId], (err, results) => {
+        if (err) return reject(err.code);
+        resolve(results);
+    })
+})
+
+Hours.getWithinDate = (startDate, endDate, userId) => new Promise((resolve, reject) => {
+    const sqlStart = new Date(startDate);
+    const sqlEnd = new Date(endDate);
+
+    if (userId === undefined) {
+        sql.query("SELECT * FROM hours WHERE time_in BETWEEN ? AND ?", [sqlStart, sqlEnd], (err, results) => {
+            if (err) return reject(err.code);
+            resolve(results);
+        })
+    } else {
+        sql.query("SELECT * FROM hours WHERE time_in BETWEEN ? AND ? AND user_id = ?", [sqlStart, sqlEnd, userId], (err, results) => {
+            if (err) return reject(err.code);
+            resolve(results);
+        })
+    }
+})
 
 Hours.remove = (id) => new Promise((resolve, reject) => {
     sql.query("DELETE FROM hours WHERE id = ?", [id], (err, result) => {
@@ -25,7 +68,7 @@ Hours.remove = (id) => new Promise((resolve, reject) => {
 })
 
 Hours.adjust = (id, fields) => new Promise((resolve, reject) => {
-    if (!fields.time_in || !fields.time_out) return reject("Incorrect POST fields!");
+    if (!fields.time_in || !fields.time_out) return reject("Incorrect body parameters!");
 
     sql.query("UPDATE hours SET time_in = ?, time_out = ? WHERE id = ?", [fields.time_in, fields.time_out, id], (err, result) => {
         if (err) return reject(err.code);
