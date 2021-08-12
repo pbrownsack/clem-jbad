28852c402938da10353e9cdfad96def97004a1d3
pbrownsack <pqtbrown@gmail.com>
2021-08-12 14:09:20 -0500

    Finish hours route/model

--- server/models/Hours.js
+++ server/models/Hours.js
@@ -36,6 +36,13 @@ Hours.getAll = (userId) => new Promise((resolve, reject) => {
     }
 })
 
+Hours.getUserTotal = (userId) => new Promise((resolve, reject) => {
+    sql.query("SELECT SUM(TIMESTAMPDIFF(HOUR, time_in, time_out)) AS total_hours FROM hours WHERE user_id = ? AND time_out IS NOT NULL", [userId], (err, results) => {
+        if (err) return reject(err.code);
+        resolve(results[0] || {});
+    })
+})
+
 Hours.getByProject = (projectId) => new Promise((resolve, reject) => {
     sql.query("SELECT * FROM hours WHERE project_id = ?", [projectId], (err, results) => {
         if (err) return reject(err.code);
@@ -44,18 +51,29 @@ Hours.getByProject = (projectId) => new Promise((resolve, reject) => {
 })
 
 Hours.getWithinDate = (startDate, endDate, userId) => new Promise((resolve, reject) => {
-    const sqlStart = new Date(startDate);
-    const sqlEnd = new Date(endDate);
-
     if (userId === undefined) {
-        sql.query("SELECT * FROM hours WHERE time_in BETWEEN ? AND ?", [sqlStart, sqlEnd], (err, results) => {
+        sql.query("SELECT * FROM hours WHERE time_in BETWEEN ? AND ?", [startDate, endDate], (err, results) => {
             if (err) return reject(err.code);
-            resolve(results);
+            resolve(results || []);
         })
     } else {
-        sql.query("SELECT * FROM hours WHERE time_in BETWEEN ? AND ? AND user_id = ?", [sqlStart, sqlEnd, userId], (err, results) => {
+        sql.query("SELECT * FROM hours WHERE time_in BETWEEN ? AND ? AND user_id = ?", [startDate, endDate, userId], (err, results) => {
             if (err) return reject(err.code);
-            resolve(results);
+            resolve(results || []);
+        })
+    }
+})
+
+Hours.getAfterDate = (startDate, userId) => new Promise((resolve, reject) => {
+    if (userId === undefined) {
+        sql.query("SELECT * FROM hours WHERE time_in >= ?", [startDate], (err, results) => {
+            if (err) return reject(err.code);
+            resolve(results || []);
+        })
+    } else {
+        sql.query("SELECT * FROM hours WHERE time_in >= ? AND user_id = ?", [startDate, userId], (err, results) => {
+            if (err) return reject(err.code);
+            resolve(results || []);
         })
     }
 })
@@ -67,13 +85,23 @@ Hours.remove = (id) => new Promise((resolve, reject) => {
     })
 })
 
-Hours.adjust = (id, fields) => new Promise((resolve, reject) => {
-    if (!fields.time_in || !fields.time_out) return reject("Incorrect body parameters!");
-
-    sql.query("UPDATE hours SET time_in = ?, time_out = ? WHERE id = ?", [fields.time_in, fields.time_out, id], (err, result) => {
-        if (err) return reject(err.code);
-        resolve(result);
-    })
+Hours.adjust = (fields) => new Promise((resolve, reject) => {
+    if (fields.time_in && !fields.time_out) {
+        sql.query("UPDATE hours SET time_in = ? WHERE id = ?", [fields.time_in, fields.id], (err, result) => {
+            if (err) return reject(err.code);
+            resolve(result);
+        })
+    } else if (fields.time_out && !fields.time_in) {
+        sql.query("UPDATE hours SET time_out = ? WHERE id = ?", [fields.time_out, fields.id], (err, result) => {
+            if (err) return reject(err.code);
+            resolve(result);
+        })
+    } else {
+        sql.query("UPDATE hours SET time_in = ?, time_out = ? WHERE id = ?", [fields.time_in, fields.time_out, fields.id], (err, result) => {
+            if (err) return reject(err.code);
+            resolve(result);
+        })
+    }
 })
 
 module.exports = Hours;
--- server/routes/v1/hours.js
+++ server/routes/v1/hours.js
@@ -1,5 +1,116 @@
 const express = require("express");
 const Hours = require("../../models/Hours");
 const router = express.Router();
+const { isAuthed, isAdmin } = require("../../middleware/auth");
+
+router.get("/", isAuthed, isAdmin, async (req, res) => {
+    try {
+        const allHours = await Hours.getAll();
+        res.send(allHours);
+    } catch (err) {
+        res.send({ error: err });
+    }
+})
+
+router.get("/user/:id/total", isAuthed, async (req, res) => {
+    try {
+        const userTotal = await Hours.getUserTotal(req.params.id);
+        res.send(userTotal);
+    } catch (err) {
+        res.send({ error: err });
+    }
+})
+
+router.get("/user/:id", isAuthed, async (req, res) => {
+    try {
+        const userHours = await Hours.getAll(req.params.id);
+        res.send(userHours);
+    } catch (err) {
+        res.send({ error: err });
+    }
+})
+
+router.get("/project/:id", isAuthed, async (req, res) => {
+    try {
+        const projectHours = await Hours.getByProject(req.params.id);
+        res.send(projectHours);
+    } catch (err) {
+        res.send({ error: err });
+    }
+})
+
+router.get("/date", isAuthed, async (req, res) => {
+    if (!req.query.start) return res.send({ error: "Invalid 'start' query parameter" });
+
+    try {
+        if (req.query.start && req.query.end) {
+            const withinHours = await Hours.getWithinDate(new Date(req.query.start), new Date(req.query.end), req.query.userId || undefined);
+            res.send(withinHours);
+        } else {
+            const afterHours = await Hours.getAfterDate(new Date(req.query.start), req.query.userId || undefined);
+            res.send(afterHours);
+        }
+    } catch (err) {
+        res.send({ error: err });
+    }
+})
+
+router.get("/:id", isAuthed, async (req, res) => {
+    try {
+        const idHours = await Hours.findById(req.params.id);
+        res.send(idHours);
+    } catch (err) {
+        res.send({ error: err });
+    }
+})
+
+router.put("/", isAuthed, async (req, res) => {
+    if (!(req.body.time_in || req.body.time_out) || !req.body.id) return res.send({ error: "Incorrect body parameters!" });
+
+    if (req.body.time_in)
+        req.body.time_in = new Date(req.body.time_in);
+    
+    if (req.body.time_out)
+        req.body.time_out = new Date(req.body.time_out);
+
+    try {
+        await Hours.adjust(req.body);
+        res.send({ message: `Successfully adjusted hour with id: ${req.body.id}` });
+    } catch (err) {
+        res.send({ error: err });
+    }
+})
+
+router.delete("/", isAuthed, async (req, res) => {
+    if (!req.body.id) return res.send({ error: "You must specify an ID to delete!" });
+
+    try {
+        await Hours.remove(req.body.id);
+        res.send({ message: `Successfully deleted hour with id: ${req.body.id}` });
+    } catch (err) {
+        res.send({ error: err });
+    }
+})
+
+router.post("/", isAuthed, async (req, res) => {
+    if (!req.body.user_id || !req.body.time_in) return res.send({ error: "Missing 'user_id', 'time_in' body parameters!" });
+
+    req.body.time_in = new Date(req.body.time_in);
+
+    if (req.body.time_out)
+        req.body.time_out = new Date(req.body.time_out);
+
+    try {
+        await Hours.createNew(req.body);
+        res.send({ message: `Successfully created new hour for user with id: ${req.body.user_id}` });
+    } catch (err) {
+        res.send({ error: err });
+    }
+})
+
+// TESTING ONLY
+router.get("/time/now", isAuthed, isAdmin, (req, res) => {
+    res.send(new Date().toISOString());
+})
 
 module.exports = router;
